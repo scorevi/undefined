4a1397435644ebbf5d0b425820793181d69f65cf
Krisisy <94095237+xyvii@users.noreply.github.com>
2025-07-19 22:07:16 +0800

    Added main page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,13 @@
 import './bootstrap';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/NavBar';
+import Main from './pages/Main';
 
 const App = () => (
     <> 
         <Navbar name="John"/> {/* Placeholder name */}
         <h1>Hello from React in Laravel!</h1>
+        <Main />
     </>
 );
 
---
+++ resources/js/pages/Main.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Main = () => {
+    return(
+        <>
+
+        Hello
+        </>
+    )
+}
+
+export default Main;
