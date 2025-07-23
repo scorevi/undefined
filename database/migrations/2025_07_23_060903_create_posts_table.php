14d84f216efb8c99a4baef6f1bffb90e132fca0d
Krisisy <94095237+xyvii@users.noreply.github.com>
2025-07-23 20:10:57 +0800

    Removed unnecesary schema

--- database/migrations/2025_07_23_060903_create_posts_table.php
+++ database/migrations/2025_07_23_060903_create_posts_table.php
@@ -29,23 +29,3 @@ return new class extends Migration
         Schema::dropIfExists('posts');
     }
 };
-
-class CreatePostViewsTable extends Migration
-{
-    public function up()
-    {
-        Schema::create('post_views', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('post_id');
-            $table->string('ip', 45);
-            $table->timestamps();
-            $table->unique(['post_id', 'ip']);
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        });
-    }
-
-    public function down()
-    {
-        Schema::dropIfExists('post_views');
-    }
-}
